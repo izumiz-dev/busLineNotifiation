073891036ffd499b4c3c5a365d3b84e9c95fd3ee
Izumi Zama <themax67@gmail.com>
2019-12-15 02:56:37 +0900

    refactor timetable data model

--- src/busNotifier.js
+++ src/busNotifier.js
@@ -1,45 +1,46 @@
 const moment = require("moment-timezone")
 moment.tz.setDefault("Asia/Tokyo")
-const timeTableUniv = require("../timetables/timeTableUniv.json")
-const timeTableBusCenter = require("../timetables/timeTableBusCenter.json")
+const timeTable = require("../timetables/timeTable_example.json")
 
 const busNotifier = event => {
-  let result = ""
-  let busTimeTable
+  const wakeWord = event.message.text
   if ([0, 6].includes(moment().day())) {
     return "現在，土曜日・日曜日に対応しておりません 😭"
   }
-  if (event.message.text === "大学" || event.message.text === "帰り") {
-    result += "大学発 - 駅方面\n"
-    busTimeTable = timeTableUniv
-  } else if (event.message.text === "バスセンター" || event.message.text === "行き") {
-    result += "バスセンター発 - 大学行\n"
-    busTimeTable = timeTableBusCenter
-  } else {
-    return "-「行き」もしくは「バスセンター」を送信すると，バスセンター発\n-「帰り」もしくは「大学」を送信すると大学発\nのバス出発時刻を返信します"
-  }
-  const findedBus = findBus(busTimeTable)
-  if (findedBus.length === 0) {
-    return "現在利用時間外です"
-  }
-  result += "1時間以内の出発予定のバス🚌💨\n"
-  findedBus.forEach((bus, index) => {
-    result += `${index + 1}：${bus.type ? bus.type === "exp" ? "急行" : "普通" : ""} ${bus.time.toString()}発 - ${bus.mins}分後\n`
-  })
-  return result.slice(0, -1)
+
+  const data = findBus({ wakeWord })
+  const sendMessageText = createMessage({ data })
+  return sendMessageText
 }
 
 
-
-const findBus = data => {
-  const res = []
-  data.forEach(bus => {
-    const next = moment().format(`YYYY-MM-DD ${bus.time}:00`)
-    if (moment(next).isBetween(moment(), moment().add(60, "m"))) {
-      res.push({ "time": moment(next).format("HH:mm"), "mins": moment(next).diff(moment(), "minutes"), "type": bus.type })
+const findBus = ({ wakeWord }) => {
+  let results = { nexts: [], destination: "", departure: "" }
+  timeTable.forEach(table => {
+    if (table.wakeWords.includes(wakeWord)) {
+      const { destination, departure } = table
+      results.destination = destination
+      results.departure = departure
+      table.timeTable.forEach(bus => {
+        const next = moment().format(`YYYY-MM-DDT${bus.time}:00`)
+        if (moment(next).isBetween(moment(), moment().add(60, "m"))) {
+          results.nexts.push({ "time": moment(next).format("HH:mm"), "mins": moment(next).diff(moment(), "minutes"), "type": bus.type })
+        }
+      })
     }
   })
-  return res
+  return results
+}
+
+const createMessage = ({ data }) => {
+  const { destination, departure } = data
+  let text = ""
+  text += "1時間以内に出発するバス🚌💨\n"
+  text += `${departure}発 - ${destination}行\n`
+  data.nexts.forEach((bus, index) => {
+    text += `${index + 1}：${bus.type ? bus.type === "exp" ? "急行" : "各停" : ""} ${bus.time.toString()}発 - ${bus.mins}分後\n`
+  })
+  return text.slice(0, -1)
 }
 
 module.exports = busNotifier
